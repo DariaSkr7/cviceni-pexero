81122b0e8be22bad98faf96d90c48e3b1f539e47
Daria Skoryk <dariaskoryk@gmail.com>
2023-10-28 22:23:30 +0200

    result

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-console.log('Funguju!')
+console.log('Funguju!');
 
-const karticky = document.querySelectorAll('.karticka')
+const karticky = document.querySelectorAll('.karticka');
 
-karticky[0].classList.remove('otocena')
+karticky.forEach((karta) => {
+  karta.addEventListener('click', () => {
+    karta.classList.toggle('otocena');
+
+    setTimeout(() => {
+      const otoceneKarty = document.querySelectorAll('.karticka:not(.otocena)');
+      if (otoceneKarty.length === 2) {
+        otoceneKarty.forEach((otocenaKarta) => {
+          console.log(otocenaKarta);
+          otocenaKarta.classList.toggle('otocena');
+        });
+      }
+    }, 500);
+  });
+});
+
+// karticky[0].classList.remove('otocena')
